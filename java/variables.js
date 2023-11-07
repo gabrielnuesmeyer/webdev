d0f6cdac5c042aedca2da84b143c3f8497410e13
Scholar <scholar@paradigm.local>
2023-11-07 15:00:18 -0700

    variables

---
+++ java/variables.js
@@ -0,0 +1,59 @@
+const myName = "gabe"
+
+ let favColor = "blue"
+
+ favColor = "red"
+
+    console.log(favColor)
+
+ let age = 17
+
+const isAlive = true;
+
+ mainperson = {
+    name: "gabe",
+    age: 17,
+    favColor: "blue",
+    favFoods: ["steak","ice creams","pizza"]
+     
+ }
+
+ 
+ const HisName = "Max"
+
+ let HisColor = "green"
+
+ let HisAge = "18"
+
+ let HisFood = ["pork","steak","potatos"]
+
+ const Isolder = true;
+
+ friend1 = {
+    HisName: "Max",
+    HisColor: "green"
+    HisAge: "18"
+    HisFood: ["pork","steak","potatos"]
+ }
+
+
+ const HerName = "bella"
+
+ let HerColor = "pink"
+
+ HerColor = "pink"
+
+    console.log(favColor)
+
+ let HerAgeage = 17
+
+ let HerFoods = ["pizza","ice cream","potatos"]
+
+const LikesPink = true;
+
+ friend2 = {
+    name: "bella",
+    HerAge: 17,
+    HerColor: "pink",
+    HerColorFoods: ["steak","ice creams","pizza"]
+ }
